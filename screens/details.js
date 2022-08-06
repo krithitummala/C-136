5db52791f530196f08945e52965de0f47242fd84
krithitummala <63346032+krithitummala@users.noreply.github.com>
2022-08-06 13:59:27 +0530

    Add files via upload

---
+++ screens/details.js
@@ -0,0 +1,10 @@
+import React from "react";
+import {View,Text,StyleSheet,Button,Alert} from "react-native"
+
+export default class DetailsScreen extends React.Component{
+    render(){
+        return(
+            <View><Text>DetailsScreen</Text></View>
+        )
+    }
+} 
---
+++ screens/home.js
@@ -0,0 +1,10 @@
+import React from "react";
+import {View,Text,StyleSheet,Button,Alert} from "react-native"
+
+export default class HomeScreen extends React.Component{
+    render(){
+        return(
+            <View><Text>HomeScreen</Text></View>
+        )
+    }
+} 
